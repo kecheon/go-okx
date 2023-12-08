9053823a8b988e27175952bc08e7cbb755a33413
kecheon <kecheon@gmail.com>
2023-12-08 13:27:33 +0900

    add get_instruments feaure

---
+++ rest/api/market/get_instruments.go
@@ -0,0 +1,51 @@
+package market
+
+import "github.com/iaping/go-okx/rest/api"
+
+func NewGetInstruments(param *GetInstrumentsParam) (api.IRequest, api.IResponse) {
+	return &api.Request{
+		Path:   "/api/v5/public/instruments",
+		Method: api.MethodGet,
+		Param:  param,
+	}, &GetInstrumentsResponse{}
+}
+
+type GetInstrumentsParam struct {
+	InstType string `url:"instType"`
+}
+
+type GetInstrumentsResponse struct {
+	api.Response
+	Data []Instrument `json:"data"`
+}
+
+type Instrument struct {
+	InstType     string `json:"instType"`
+	InstId       string `json:"instId"`
+	InstFamily   string `json:"instFamily"`
+	Uly          string `json:"uly"`
+	Category     string `json:"category"`
+	BaseCcy      string `json:"baseCcy"`
+	QuoteCcy     string `json:"quoteCcy"`
+	SettleCcy    string `json:"settleCcy"`
+	CtVal        string `json:"ctVal"`
+	CtMult       string `json:"ctMult"`
+	CtValCcy     string `json:"ctValCcy"`
+	OptType      string `json:"optType"`
+	Stk          string `json:"stk"`
+	ListTime     string `json:"listTime"`
+	ExpTime      string `json:"expTime"`
+	Lever        string `json:"lever"`
+	TickSz       string `json:"tickSz"`
+	LotSz        string `json:"lotSz"`
+	MinSz        string `json:"minSz"`
+	CtType       string `json:"ctType"`
+	Alias        string `json:"alias"`
+	State        string `json:"state"`
+	MaxLmtSz     string `json:"maxLmtSz"`
+	MaxMktSz     string `json:"maxMktSz"`
+	MaxTwapSz    string `json:"maxTwapSz"`
+	MaxIcebergSz string `json:"maxIcebergSz"`
+	MaxTriggerSz string `json:"maxTriggerSz"`
+	MaxStopSz    string `json:"maxStopSz"`
+}
